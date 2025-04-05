3a53facc0d1006ee2163c4eaa047cf85f2d30d77
Thomas Schmidt <t.schmidt@md-network.de>
2025-04-05 23:15:13 +0200

    Added ATEM interface skeleton andfixed FSWatcher + Config managment

---
+++ src/LoupedeckAtemControlerPlugin/ATEM/AtemControlInterface.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Loupedeck.LoupedeckAtemControlerPlugin.Helpers;
+
+namespace Loupedeck.LoupedeckAtemControlerPlugin.ATEM
+{
+    public class AtemControlInterface
+    {
+
+        private readonly StillImageData _imageData;
+
+        public AtemControlInterface(StillImageData imageData)
+        {
+            this._imageData = imageData;
+        }
+
+        public Boolean setStillImage(String uri, String fileName) {
+            PluginLog.Info($">>>> [AtemControlInterface.setStillImage()] Sending Image <{fileName}> to <{uri}>");
+            return true;
+        }
+    }
+}
+
---
+++ src/LoupedeckAtemControlerPlugin/Actions/ConfigAtemDummyCommand.cs
@@ -0,0 +1,27 @@
+namespace Loupedeck.LoupedeckAtemControlerPlugin
+{
+    using System;
+
+
+    public class ConfigAtemDummyCommand : PluginDynamicCommand
+    {
+
+
+        private LoupedeckAtemControlerPlugin _plugin => (LoupedeckAtemControlerPlugin)this.Plugin;
+
+
+        public ConfigAtemDummyCommand()
+               : base(displayName: "Set ATEM URI", description: "Lets one configure the ATEM IP address or host name", groupName: "Commands")
+                    => this.MakeProfileAction("text;Enter ATEM Name or IP:");
+
+
+
+        protected override void RunCommand(String actionParameter)
+        {
+            this._plugin.SetPluginSetting("AtemURI", actionParameter, false);
+            this._plugin.stillImageData.AtemURI = actionParameter;
+
+        }
+
+    }
+}
